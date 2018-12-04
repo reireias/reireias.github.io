90e8eee3b0f8a49e1a51c198ebd0a5d41ce455bf
reireias <reireias@gmail.com>
2018-12-04 23:59:47 +0900

    create project

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-xs-center">
+        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
