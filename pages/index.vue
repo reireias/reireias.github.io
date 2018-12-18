5c5fb2cf7158613643c380c820e2a93c857d436e
reireias <reireias@gmail.com>
2018-12-18 21:09:51 +0900

    add font awesome

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <skill />
+  <div>
+    <v-img src="icon.jpg" height="200px" width="200px" />
+    <p><i class="fab fa-twitter" aria-hidden="true"></i>@reirei_As</p>
+    <p><i class="fab fa-github"></i>reireias</p>
+    <skill />
+  </div>
 </template>
 
 <script>
