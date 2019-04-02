b3c34e525e4485dfee35dffb5d144242bbc2831f
reireias <reireias@gmail.com>
2019-04-02 22:38:11 +0900

    add ogp settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,53 @@ export default {
         hid: 'description',
         name: 'description',
         content: pkg.description
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'reireias portfolio'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://reireias.github.io'
+      },
+      { hid: 'og:title', property: 'og:title', content: 'reireias portfolio' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: pkg.description
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://reireias.github.io/ogp.png'
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary large image'
+      },
+      {
+        hid: 'twitter:site',
+        name: 'twitter:site',
+        content: '@reirei_As'
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: pkg.name
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: pkg.description
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://reireias.github.io/ogp.png'
       }
     ],
     link: [
