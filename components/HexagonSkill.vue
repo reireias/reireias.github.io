e97245a2750e90de19358b31be81f2c3ec38d2bd
reireias <reireias@gmail.com>
2019-06-03 23:14:29 +0900

    update skill grid

--- components/HexagonSkill.vue
+++ components/HexagonSkill.vue
@@ -1,33 +1,17 @@
 <template>
-  <v-container grid-list-md>
-    <h1>Skill</h1>
-    <v-select
-      v-model="sort"
-      :items="sorters"
-      label="Sort"
-      @change="sortChange"
-    />
-    <transition-group name="cards" tag="div" class="layout row wrap">
-      <v-flex
-        v-for="skill in skills"
-        :key="skill.name"
-        class="skill"
-        xs4
-        sm3
-        md2
-        text-xs-center
-      >
-        <v-card>
-          <center>
-            <v-img :src="skill.image" height="100px" width="100px" contain />
-          </center>
-          <v-card-text class="name">
-            {{ skill.name }}
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </transition-group>
-  </v-container>
+  <div class="body">
+    <ul id="hexGrid">
+      <li v-for="skill in skills" :key="skill.name" class="hex">
+        <div class="hexIn">
+          <a class="hexLink" href="#">
+            <img :src="skill.image" alt="" />
+            <h1>{{ skill.name }}</h1>
+            <p><br /></p>
+          </a>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -302,10 +286,183 @@ export default {
 </script>
 
 <style>
-.name {
-  padding: 5px;
+* {
+  margin: 0;
+  padding: 0;
 }
-.cards-move {
-  transition: transform 1s;
+body {
+  background: rgb(123, 158, 158);
+}
+#hexGrid {
+  display: flex;
+  flex-wrap: wrap;
+  width: 90%;
+  margin: 0 auto;
+  overflow: hidden;
+  font-family: 'Raleway', sans-serif;
+  font-size: 15px;
+  list-style-type: none;
+}
+
+.hex {
+  position: relative;
+  visibility: hidden;
+  outline: 1px solid transparent; /* fix for jagged edges in FF on hover transition */
+}
+.hex::after {
+  content: '';
+  display: block;
+  padding-bottom: 86.602%; /* =  100 / tan(60) * 1.5 */
+}
+.hexIn {
+  position: absolute;
+  width: 96%;
+  padding-bottom: 110.851%; /* =  width / sin(60) */
+  margin: 0 2%;
+  overflow: hidden;
+  visibility: hidden;
+  outline: 1px solid transparent; /* fix for jagged edges in FF on hover transition */
+  -webkit-transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
+  -ms-transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
+  transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
+}
+.hexIn * {
+  position: absolute;
+  visibility: visible;
+  outline: 1px solid transparent; /* fix for jagged edges in FF on hover transition */
+}
+.hexLink {
+  display: block;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  color: #fff;
+  overflow: hidden;
+  -webkit-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+  -ms-transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+  transform: skewY(-30deg) rotate3d(0, 0, 1, 60deg);
+}
+
+/*** HEX CONTENT **********************************************************************/
+.hex img {
+  background-color: #fff;
+  left: -100%;
+  right: -100%;
+  width: auto;
+  height: 100%;
+  margin: 0 auto;
+  padding: 20%;
+  -webkit-transform: rotate3d(0, 0, 0, 0deg);
+  -ms-transform: rotate3d(0, 0, 0, 0deg);
+  transform: rotate3d(0, 0, 0, 0deg);
+}
+
+.hex h1,
+.hex p {
+  width: 100%;
+  padding: 5%;
+  box-sizing: border-box;
+  background-color: rgba(0, 128, 128, 0.8);
+  font-weight: 300;
+  -webkit-transition: -webkit-transform 0.2s ease-out, opacity 0.3s ease-out;
+  transition: transform 0.2s ease-out, opacity 0.3s ease-out;
+}
+.hex h1 {
+  bottom: 50%;
+  padding-top: 50%;
+  font-size: 1.5em;
+  z-index: 1;
+  -webkit-transform: translate3d(0, -100%, 0);
+  -ms-transform: translate3d(0, -100%, 0);
+  transform: translate3d(0, -100%, 0);
+}
+.hex h1::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 45%;
+  width: 10%;
+  text-align: center;
+  border-bottom: 1px solid #fff;
+}
+.hex p {
+  top: 50%;
+  padding-bottom: 50%;
+  -webkit-transform: translate3d(0, 100%, 0);
+  -ms-transform: translate3d(0, 100%, 0);
+  transform: translate3d(0, 100%, 0);
+}
+
+/*** HOVER EFFECT  **********************************************************************/
+.hexLink:hover h1,
+.hexLink:focus h1,
+.hexLink:hover p,
+.hexLink:focus p {
+  -webkit-transform: translate3d(0, 0, 0);
+  -ms-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+/*** HEXAGON SIZING AND EVEN ROW INDENTATION *****************************************************************/
+@media (min-width: 1201px) {
+  /* <- 5-4  hexagons per row */
+  #hexGrid {
+    padding-bottom: 4.4%;
+  }
+  .hex {
+    width: 10%; /* = 100 / 5 */
+  }
+  .hex:nth-child(19n + 11) {
+    /* first hexagon of even rows */
+    margin-left: 5%; /* = width of .hex / 2  to indent even rows */
+  }
+}
+
+@media (max-width: 1200px) and (min-width: 901px) {
+  /* <- 4-3  hexagons per row */
+  #hexGrid {
+    padding-bottom: 5.5%;
+  }
+  .hex {
+    width: 25%; /* = 100 / 4 */
+  }
+  .hex:nth-child(7n + 5) {
+    /* first hexagon of even rows */
+    margin-left: 12.5%; /* = width of .hex / 2  to indent even rows */
+  }
+}
+
+@media (max-width: 900px) and (min-width: 601px) {
+  /* <- 3-2  hexagons per row */
+  #hexGrid {
+    padding-bottom: 7.4%;
+  }
+  .hex {
+    width: 33.333%; /* = 100 / 3 */
+  }
+  .hex:nth-child(5n + 4) {
+    /* first hexagon of even rows */
+    margin-left: 16.666%; /* = width of .hex / 2  to indent even rows */
+  }
+}
+
+@media (max-width: 600px) {
+  /* <- 2-1  hexagons per row */
+  #hexGrid {
+    padding-bottom: 11.2%;
+  }
+  .hex {
+    width: 50%; /* = 100 / 3 */
+  }
+  .hex:nth-child(3n + 3) {
+    /* first hexagon of even rows */
+    margin-left: 25%; /* = width of .hex / 2  to indent even rows */
+  }
+}
+
+@media (max-width: 400px) {
+  #hexGrid {
+    font-size: 13px;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
     <div class="relative">
-      <no-ssr>
-        <!-- eslint-disable vue/attribute-hyphenation -->
-        <vue-particles
-          class="particles"
-          color="#dedede"
-          :particleOpacity="0.7"
-          linesColor="#ffffff"
-          :particlesNumber="80"
-          shapeType="circle"
-          :particleSize="4"
-          :linesWidth="1"
-          :lineLinked="true"
-          :lineOpacity="0.4"
-          :linesDistance="150"
-          :moveSpeed="3"
-          :hoverEffect="false"
-          :clickEffect="true"
-          clickMode="repulse"
-        />
-        <!-- eslint-enable -->
-      </no-ssr>
       <div class="profile">
         <center>
           <v-avatar size="200">
@@ -54,15 +33,15 @@
         </center>
       </div>
     </div>
-    <skill />
+    <hexagon-skill />
   </div>
 </template>
 
 <script>
-import Skill from '~/components/Skill.vue'
+import HexagonSkill from '~/components/HexagonSkill.vue'
 export default {
   components: {
-    Skill
+    HexagonSkill
   }
 }
 </script>
@@ -70,22 +49,6 @@ export default {
 <style>
 h1 {
   font-weight: normal;
-}
-.particles {
-  width: 100%;
-  height: 100%;
-  background-image: linear-gradient(90deg, #fc00ff, #00dbde);
-}
-.relative {
-  position: relative;
-  height: 500px;
-}
-.profile {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateY(-50%) translateX(-50%);
-  -webkit-transform: translateY(-50%) translateX(-50%);
 }
 .profile h1 {
   font-weight: normal;
