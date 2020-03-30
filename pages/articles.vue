27dd0189aa9cceea002c3f19f4a7d3f8bc41df40
reireias <reireias@gmail.com>
2020-03-30 22:17:31 +0900

    renewal

---
+++ pages/articles.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <div class="display-3">Article</div>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" sm="6">
+        <v-card>
+          <div style="width: 100%; height: 300px; background: gray;"></div>
+          <v-card-title class="bottom-border-primary title">
+            ラズパイ4で作るディスプレイ付きKubernetesクラスター
+          </v-card-title>
+          <v-card-text>
+            TODO
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card>
+          <v-card-title class="bottom-border-primary title">
+            Firebase + Nuxt.js + RaspberryPiで作る猫監視システム
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- test/pages/profile.test.js
+++ test/pages/profile.test.js
@@ -1,13 +1,13 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import target from '@/pages/index'
+import target from '@/pages/profile'
 
 Vue.use(Vuetify)
 const localVue = createLocalVue()
 localVue.use(Vuetify)
 
-describe('index', () => {
+describe('profile', () => {
   it('should be vue instance', () => {
     const wrapper = shallowMount(target, {
       localVue,
