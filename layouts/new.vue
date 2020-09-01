d879234a84c1211091dc03252b0df31ca4c258d1
reireias <reireias@gmail.com>
2020-09-02 00:16:59 +0900

    add new layout and template page

---
+++ layouts/new.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+          <div class="display-4">Title(TODO: get from store)</div>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12" sm="4">
+            <nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/template.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>card title</v-card-title>
+    <v-card-subtitle>card sub title</v-card-subtitle>
+    <v-card-text>text</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  layout: 'new',
+}
+</script>
