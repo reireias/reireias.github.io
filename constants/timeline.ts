14450f0459fb3b9245e09aec60b6be39a5bbe1d5
reireias <reireias@gmail.com>
2020-09-28 01:08:24 +0900

    use v-timeline in job page

---
+++ constants/timeline.ts
@@ -0,0 +1,40 @@
+const timeline = [
+  {
+    color: 'secondary',
+    year: 2007,
+    title: '筑波大学 入学',
+    text: '理工学群 工学システム学類',
+  },
+  {
+    color: 'secondary',
+    year: 2011,
+    title: '同大学 卒業',
+    text: '学位（工学）を取得',
+  },
+  {
+    color: 'accent',
+    year: 2011,
+    title: '筑波大学大学院 入学',
+    text: 'システム情報工学研究科 リスク工学専攻',
+  },
+  {
+    color: 'accent',
+    year: 2013,
+    title: '同大学院 卒業',
+    text: '修士（工学）を取得',
+  },
+  {
+    color: 'warning',
+    year: 2013,
+    title: '富士ゼロックス株式会社',
+    text: 'ソリューション開発部にて、BtoBのSaaSを開発',
+  },
+  {
+    color: 'warning',
+    year: 2019,
+    title: 'メドピア株式会社',
+    text: 'SREとしてアーキテクトやインフラ構築を担当',
+  },
+]
+
+export default timeline
