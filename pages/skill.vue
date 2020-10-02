144dcbbfa79e7845c9c6d92eaaac019089bb378d
reireias <reireias@gmail.com>
2020-10-02 22:55:17 +0900

    update skill page layout

--- pages/skill.vue
+++ pages/skill.vue
@@ -1,26 +1,43 @@
 <template>
-  <div>
+  <div class="skill-page">
     <page-title title="Skill"></page-title>
 
-    <sre-skill-card></sre-skill-card>
-
-    <br />
-
-    <web-skill-card></web-skill-card>
+    <v-expansion-panels class="panel" multiple>
+      <v-expansion-panel v-for="skill in skills" :key="skill.header">
+        <v-expansion-panel-header class="title">{{
+          skill.header
+        }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p v-for="(line, i) in skill.content" :key="i">{{ line }}</p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import PageTitle from '@/components/PageTitle.vue'
-import SreSkillCard from '~/components/SreSkillCard.vue'
-import WebSkillCard from '~/components/WebSkillCard.vue'
+import skills from '@/constants/skills'
 
 export default defineComponent({
   components: {
     PageTitle,
-    SreSkillCard,
-    WebSkillCard,
+  },
+  setup() {
+    return { skills }
   },
 })
 </script>
+
+<style lang="scss">
+.skill-page {
+  .panel {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .v-expansion-panel {
+    margin-top: 16px;
+  }
+}
+</style>
