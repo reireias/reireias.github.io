15df199adcbb79012e3df2c6af015f2a7f9c7b8a
reireias <reireias@gmail.com>
2021-03-10 18:19:49 +0900

    update .eslintrc.js
    https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
     'prettier',
-    'prettier/vue',
   ],
   plugins: ['prettier'],
   // add your custom rules here
