db74aa0ee55ac91238992910949ba43ec6aa1dee
reireias <reireias@users.noreply.github.com>
2022-02-17 02:29:51 +0000

    deploy: 3c325ed438115178da0934754acbf134f9709426

--- _nuxt/static/1645064860/sandbox/anime/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645064860",layout:"gridless",error:a,serverRendered:true,routePath:"\u002Fsandbox\u002Fanime",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
