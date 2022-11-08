db5b8eda85f53aae88ad5909a8fa759fab579a32
reireias <reireias@gmail.com>
2022-11-08 16:40:40 +0900

    fix: page title margin

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,6 +1,11 @@
 <template>
   <v-row justify="center">
-    <div class="display-3 primary--text">{{ title }}</div>
+    <div
+      class="display-3 primary--text"
+      style="margin-top: 30px; margin-bottom: 10px"
+    >
+      {{ title }}
+    </div>
   </v-row>
 </template>
 
