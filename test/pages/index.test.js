cfeeac611f7c544a2ac66a2be81c43bc80e5dce4
reireias <reireias@gmail.com>
2026-07-29 21:58:26 +0900

    feat: shorten home animation and add avatar icon animation (#482)

---
+++ test/pages/index.test.js
@@ -0,0 +1,26 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import CompositionApi from '@vue/composition-api'
+import target from '@/pages/index'
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+localVue.use(CompositionApi)
+
+describe('index', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('should be vue instance', () => {
+    const wrapper = shallowMount(target, {
+      localVue,
+      vuetify,
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
