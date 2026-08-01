0729c8c30c2ef25c2ebadf9bf18023f76f2072e4
reireias <reireias@gmail.com>
2026-08-01 12:48:32 +0900

    perf(e2e): use system chrome channel and skip browser download (#529)

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+      },
     },
   ],
   webServer: {
